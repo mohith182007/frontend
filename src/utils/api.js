bec8e275880c4a755471f25e37aef9a740f74797
jackrookie-18 <gangkoyhi@gmail.com>
2025-10-25 10:53:59 +0530

    Initial commit: survey frontend ready for Vercel deployment

---
+++ src/utils/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://survey-server-9ill.onrender.com/api';
+
+export const submitUserInfo = async (userData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/survey/user`, userData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const submitSurveyResponse = async (surveyData) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/survey/submit`, surveyData);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const getUserData = async (userId) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/survey/user/${userId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
+
+export const getSurveyResponse = async (responseId) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/survey/response/${responseId}`);
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error;
+  }
+};
